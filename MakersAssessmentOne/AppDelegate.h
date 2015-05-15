02355af3b0ae95b28cf25b031fa8c5eb6cea6c93
Mark Porcella <markporcella67@gmail.com>
2015-05-15 09:14:23 -0700

    Initial Commit

---
+++ MakersAssessmentOne/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  MakersAssessmentOne
+//
+//  Created by Mark Porcella on 5/15/15.
+//  Copyright (c) 2015 Mark Porcella. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ MakersAssessmentOne/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  MakersAssessmentOne
+//
+//  Created by Mark Porcella on 5/15/15.
+//  Copyright (c) 2015 Mark Porcella. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
